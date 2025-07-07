7be160aeb55620c03497893dfe59f411d775cd3e
Jenna-Deamer <JennaDeamer@gmail.com>
2025-07-06 21:34:15 -0400

    Add basic styles, Hide labels until weather data is displayed,

--- src/display.js
+++ src/display.js
@@ -1,11 +1,16 @@
 export function displayWeatherData(processedWeatherData) {
+  const resultsContainer = document.querySelectorAll(".results-container");
   const cityLabel = document.querySelector("#city-label");
   const descriptionLabel = document.querySelector("#desc-label");
-  const tempLabel = document.querySelector('#temp-label');
-  const conditionsLabel = document.querySelector('#conditions-label')
+  const tempLabel = document.querySelector("#temp-label");
+  const conditionsLabel = document.querySelector("#conditions-label");
 
   cityLabel.textContent = processedWeatherData.address;
   descriptionLabel.textContent = processedWeatherData.description;
-  tempLabel.textContent = processedWeatherData.temp;
+  tempLabel.textContent = processedWeatherData.temp + "°F";
   conditionsLabel.textContent = processedWeatherData.conditions;
+
+  resultsContainer.forEach(result => {
+    result.classList.remove('hidden');
+  });
 }
